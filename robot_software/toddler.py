d248873f8702ba8dfa943dead56aa8e82ea650bc
cd6 <s1616573@sms.ed.ac.uk>
2019-01-28 15:47:54 +0000

    added toddler

---
+++ robot_software/toddler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import time
+import numpy
+import cv2
+
+class Toddler:
+
+    def __init__(self, IO):
+        print('[Toddler] I am toddler playing in a sandbox')
+        self.camera = IO.camera.initCamera('pi', 'low')
+        self.getInputs = IO.interface_kit.getInputs
+        self.getSensors = IO.interface_kit.getSensors
+        self.mc = IO.motor_control
+        self.sc = IO.servo_control
+
+    def control(self):
+        print('{}\t{}'.format(self.getSensors(), self.getInputs()))
+        self.mc.setMotor(1,1)
+        time.sleep(0.5)
+
+    def vision(self):
+        image = self.camera.getFrame()
+        self.camera.imshow('Camera', image)
+        time.sleep(0.05)
