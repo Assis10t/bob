56c1d074fd6e98fb473831ba60519b94f25516fd
cd6 <s1616573@sms.ed.ac.uk>
2019-01-29 14:59:28 +0000

    move robot forward using both motors

--- robot_software/toddler.py
+++ robot_software/toddler.py
@@ -5,7 +5,9 @@ import numpy
 import cv2
 
 class Toddler:
-    MOTOR_FORWARD = 1
+    MOTOR_SIDE = 1
+    MOTOR_FRONT_LEFT = 2
+    MOTOR_FRONT_RIGHT = 3
 
     def __init__(self, IO):
         print('[Toddler] I am toddler playing in a sandbox')
@@ -17,8 +19,10 @@ class Toddler:
 
     def control(self):
         print('{}\t{}'.format(self.getSensors(), self.getInputs()))
-        self.mc.setMotor(self.MOTOR_FORWARD, 1)
+        self.mc.setMotor(self.MOTOR_FRONT_LEFT, 1)
+        self.mc.setMotor(self.MOTOR_FRONT_RIGHT, 1)
         time.sleep(0.5)
+
 
     def vision(self):
         image = self.camera.getFrame()
