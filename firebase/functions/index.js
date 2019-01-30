1049620baedbc839fd95d30a70c20d047d198c6e
Frederick Bawden <frederickbawden@c113122.wlan.net.ed.ac.uk>
2019-01-30 19:24:25 +0000

    Changes to index.js

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,10 +1,6 @@
 const functions = require('firebase-functions');
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.createUser = functions.firestore
+exports.onCreate = functions.firestore
     .document('orders/{orderID}')
     .onCreate((snap, context) => {
       return snap.ref.set({
