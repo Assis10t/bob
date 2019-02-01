370e5a87847a92de099eacd57cee50dc3b4295ec
Alexander Rader <ap.rader@yahoo.com>
2019-02-01 14:27:35 +0000

    detect colour

--- ev3/detectLine.py
+++ ev3/detectLine.py
@@ -8,16 +8,20 @@ class detectLine:
         self.shut_down = False
 
     def run(self):
-        #cs = ev3.ColorSensor()
-        #assert cs.connected
+        cs = ev3.ColorSensor()
+        assert cs.connected
 
         #cs.mode = 'COL-REFLECT' # measure light intensity
+        cs.mode = 'COL-COLOR' # measure colour
+        while(True):
+            yeet = cs.value()
+            print(yeet)
 
         # motors
         flm = ev3.LargeMotor('outA') # front-left motor
         frm = ev3.LargeMotor('outB') # front-right motor
         sm = ev3.LargeMotor('outC')  # sidemotor
-        sm.run_timed(speed_sp=300, time_sp=1000)
+        #sm.run_timed(speed_sp=300, time_sp=1000)
 
 
 # Main function
