5d2b372603456b9c52c00988a7e6763f42be03e3
Alexander Rader <ap.rader@yahoo.com>
2019-02-01 13:56:02 +0000

    makes BOB sidestep

--- ev3/detectLine.py
+++ ev3/detectLine.py
@@ -8,13 +8,19 @@ class detectLine:
         self.shut_down = False
 
     def run(self):
-        cs = ev3.ColorSensor()
-        assert cs.connected
+        #cs = ev3.ColorSensor()
+        #assert cs.connected
 
-        cs.mode = 'COL-REFLECT' # measure light intensity
+        #cs.mode = 'COL-REFLECT' # measure light intensity
 
         # motors
-        flm = ev3.LargeMotor('outA') # forward-left motor
-        frm = ev3.LargeMotor('outB') # forward-right motor
+        flm = ev3.LargeMotor('outA') # front-left motor
+        frm = ev3.LargeMotor('outB') # front-right motor
         sm = ev3.LargeMotor('outC')  # sidemotor
         sm.run_timed(speed_sp=300, time_sp=1000)
+
+
+# Main function
+if __name__ == "__main__":
+        robot = detectLine()
+        robot.run()
