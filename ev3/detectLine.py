0f10cfef7895d8265ff287bb12ee55bcdf99a5d7
cd6 <s1616573@sms.ed.ac.uk>
2019-02-01 18:46:18 +0000

    move but can't stay on line

--- ev3/detectLine.py
+++ ev3/detectLine.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 import ev3dev.ev3 as ev3
+from time import sleep
 
 
 class DetectLine:
+    # From https://gist.github.com/CS2098/ecb3a078ed502c6a7d6e8d17dc095b48
     # Constructor
     def __init__(self):
         self.btn = ev3.Button()
@@ -21,19 +23,21 @@ class DetectLine:
         sm = ev3.LargeMotor('outC')   # side motor
         #sm.run_timed(speed_sp=300, time_sp=1000)
 
-        speed = 360 / 4  # deg/sec, [-1000, 1000]
-        dt = 500  # milliseconds
-        # stop_action = "coast"
+        speed = 360 / 2.5   # deg/sec, [-1000, 1000]
+        dt = 250  # milliseconds
+        stop_action = "coast"
 
+        # TODO: tune parameters, p then d then i (i will probably be very small)
         # PID tuning
-        Kp = 1  # proportional gain
-        Ki = 0  # integral gain
-        Kd = 0  # derivative gain
+        Kp = 2.5  # proportional gain   largest
+        Ki = 0  # integral gain       medium
+        Kd = 0.1  # derivative gain   lowest
 
         integral = 0
         previous_error = 0
 
         # initial measurement when using one sensor to callibrate
+        # place sensor left half on white, right half on black
         target_value = cs.value()
 
         # Start the main loop
@@ -48,7 +52,50 @@ class DetectLine:
             # u negative: too dark,   turn left
 
             u = (Kp * error) + (Ki * integral) + (Kd * derivative)
-            print(u)
+
+            # limit u to safe values: [-1000, 1000] deg/sec
+            if speed + abs(u) > 1000:
+                if u >= 0:
+                    u = 1000 - speed
+                else:
+                    u = speed - 1000
+
+            # run motors
+            if u >= 0:
+                # wheels are now at back so reverse motors
+                frm.run_timed(time_sp=dt, speed_sp=-(speed + u))
+                flm.run_timed(time_sp=dt, speed_sp=-(speed - u))
+                sleep(dt / 1000)
+            else:
+                frm.run_timed(time_sp=dt, speed_sp=-(speed - u))
+                flm.run_timed(time_sp=dt, speed_sp=-(speed + u))
+                sleep(dt / 1000)
+
+            previous_error = error
+
+            # Check if buttons pressed (for pause or stop)
+            if self.btn.down:  # Stop
+                print("Exit program... ")
+                self.shut_down = True
+            elif self.btn.left:  # Pause
+                print("[Pause]")
+                self.pause()
+
+            # 'Pause' method
+
+    def pause(self, pct=0.0, adj=0.01):
+        while self.btn.right or self.btn.left:  # ...wait 'right' button to unpause
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER, pct)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER, pct)
+            if (pct + adj) < 0.0 or (pct + adj) > 1.0:
+                adj = adj * -1.0
+            pct = pct + adj
+
+        print("[Continue]")
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+
+
 
 
 # Main function
