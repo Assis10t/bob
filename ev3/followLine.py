da42e88dd9268b8a1cc7cc88e5498b14c8e192b8
Alexander Rader <ap.rader@yahoo.com>
2019-02-03 16:55:29 +0000

    follow line decently

--- ev3/followLine.py
+++ ev3/followLine.py
@@ -1,112 +1,102 @@
 #! /usr/bin/env python3
 import ev3dev.ev3 as ev3
+import logging
 from time import sleep
 
 
-class DetectLine:
+class FollowLine:
     # From https://gist.github.com/CS2098/ecb3a078ed502c6a7d6e8d17dc095b48
+    MOTOR_SPEED = 700
+    KP = 20
+    KD = 0.1  # derivative gain   medium
+    KI = 0  # integral gain       lowest
+
+    DT = 50  # milliseconds  -  represents change in time since last sensor reading/movement
+
     # Constructor
     def __init__(self):
         self.btn = ev3.Button()
         self.shut_down = False
 
-    def run(self):
-        csbl = ev3.ColorSensor('in2')  # colour sensor back left
-        csbr = ev3.ColorSensor('in1')  # colour sensor back right
-        assert csbl.connected
-        assert csbr.connected
+    def onLine(self, sensor_value, position):
+        if position == 'left':
+            return sensor_value < 30
+        if position == 'right':
+            return sensor_value < 40
+        logging.error("onLine: wrong position value for sensor")
+        return False
 
-        #csbl.mode = 'COL-REFLECT' # measure light intensity
-        #csbl.mode = 'COL-COLOR' # measure colour
-        #csbr.mode = 'COL-COLOR'  # measure colour
-
-        csbl.mode = 'COL-REFLECT'  # measure light intensity
-        csbr.mode = 'COL-REFLECT'  # measure light intensity
-
-        # motors
-        flm = ev3.LargeMotor('outA')  # front-left motor
-        frm = ev3.LargeMotor('outB')  # front-right motor
-        sm = ev3.LargeMotor('outC')   # side motor
-        #sm.run_timed(speed_sp=300, time_sp=1000)
-
-        speed = 360 / 2   # deg/sec, [-1000, 1000]
-        dt = 50  # milliseconds  -  represents change in time since last sensor reading/movement
-        stop_action = "coast"
-
-        # TODO: tune parameters, p then d then i (i will probably be very small)
-        # PID tuning
-        Kp = 2.5  # proportional gain   largest
-        Ki = 0  # integral gain       medium
-        Kd = 0.1  # derivative gain   lowest
-
+    def correctTrajectory(self, csfl, csfr, lm, rm):
         integral = 0
         previous_error = 0
 
-        # initial measurement when using one sensor to callibrate
-        # place sensor left half on white, right half on black
-        #target_value = cs.value()
-
-        # Start the main loop
         while not self.shut_down:
-            # Calculate steering using PID algorithm
-            error = csbl.value() - csbr.value()
-            integral += (error * dt)
-            derivative = (error - previous_error) / dt
+            lval = csfl.value()
+            rval = csfr.value()
+            error = lval - rval - 10
+            logging.info("PID error: ", error)
+            integral += (error * self.DT)
+            derivative = (error - previous_error) / self.DT
 
             # u zero:     on target,  drive forward
             # u positive: too bright, turn right
             # u negative: too dark,   turn left
-            # u is torque (See IVR lecture on Control
-            u = (Kp * error) + (Ki * integral) + (Kd * derivative)
+            # u is torque (See IVR lecture on Control)
+            u = (self.KP * error) + (self.KI * integral) + (self.KD* derivative)
 
             # limit u to safe values: [-1000, 1000] deg/sec
-            if speed + abs(u) > 1000:  # reduce u if speed and torque are too high
+            if self.MOTOR_SPEED + abs(u) > 1000:  # reduce u if speed and torque are too high
                 if u >= 0:
-                    u = 1000 - speed
+                    u = 1000 - self.MOTOR_SPEED
                 else:
-                    u = speed - 1000
+                    u = self.MOTOR_SPEED - 1000
 
             # run motors
-            if u >= 0:
-                # wheels are now at back so reverse motors
-                frm.run_timed(time_sp=dt, speed_sp=-(speed + u))
-                flm.run_timed(time_sp=dt, speed_sp=-(speed - u))
-                sleep(dt / 1000)
-            else:
-                frm.run_timed(time_sp=dt, speed_sp=-(speed + u))
-                flm.run_timed(time_sp=dt, speed_sp=-(speed - u))
-                sleep(dt / 1000)
+            lm.run_timed(time_sp=self.DT, speed_sp=-(self.MOTOR_SPEED + u))
+            rm.run_timed(time_sp=self.DT, speed_sp=-(self.MOTOR_SPEED - u))
+            sleep(self.DT / 1000)
 
-            #print("u {}".format(u))
-            #print("frm {}".format(frm.speed_sp))
-            #print("flm {}\n".format(flm.speed_sp))
+
+            print("u {}".format(u))
+            print("lm {}\n".format(lm.speed_sp))
+            print("rm {}".format(rm.speed_sp))
+            print("PID:", lval, rval)
 
             previous_error = error
 
-            # Check if buttons pressed (for pause or stop)
-            if self.btn.down:  # Stop
-                print("Exit program... ")
-                self.shut_down = True
-            elif self.btn.left:  # Pause
-                print("[Pause]")
-                self.pause()
+    def run(self):
 
+        # colour sensors
+        csfl = ev3.ColorSensor('in1')  # colour sensor front left
+        csfr = ev3.ColorSensor('in2')  # colour sensor front right
+        assert csfl.connected
+        assert csfr.connected
+        csfl.mode = 'COL-REFLECT'  # measure light intensity
+        csfr.mode = 'COL-REFLECT'  # measure light intensity
 
-    # 'Pause' method
-    def pause(self, pct=0.0, adj=0.01):
-        while self.btn.right or self.btn.left:  # ...wait 'right' button to unpause
-            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER, pct)
-            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER, pct)
-            if (pct + adj) < 0.0 or (pct + adj) > 1.0:
-                adj = adj * -1.0
-            pct = pct + adj
+        # motors
+        lm = ev3.LargeMotor('outA')  # left motor
+        rm = ev3.LargeMotor('outC')  # right motor
+        assert lm.connected
+        assert rm.connected
+        self.correctTrajectory(csfl, csfr, lm, rm)
 
-        print("[Continue]")
-        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
-        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+        """
+
+        lm.run_forever(speed_sp=-self.MOTOR_SPEED)
+        rm.run_forever(speed_sp=-self.MOTOR_SPEED)
+        while not self.shut_down:
+            print(csfl.value(), csfr.value())
+            if self.onLine(csfl.value(), 'left') or self.onLine(csfr.value(), 'right'):   # left sensor is on the line
+                lm.stop()
+                rm.stop()
+                self.correctTrajectory(csfl, csfr, lm, rm)
+        """
+        rm.stop()
+        lm.stop()
 
 
 # Main function
 if __name__ == "__main__":
-        robot = DetectLine()
-        robot.run()
+    robot = FollowLine()
+    robot.run()
