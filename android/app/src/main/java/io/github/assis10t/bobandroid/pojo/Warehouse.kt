9819c316737588266092663b9f7027dc5e3acf88
Oktay Sen <senokt.16@gmail.com>
2019-02-25 15:22:44 +0000

    Added recent small change to api v2 to app

--- android/app/src/main/java/io/github/assis10t/bobandroid/pojo/Warehouse.kt
+++ android/app/src/main/java/io/github/assis10t/bobandroid/pojo/Warehouse.kt
@@ -2,6 +2,7 @@ package io.github.assis10t.bobandroid.pojo
 
 class Warehouse (
     val _id: String? = null,
+    val name: String = "Unnamed Warehouse",
     val merchantId: String? = null,
     val location: Location? = null,
     val items: List<Item>? = null
