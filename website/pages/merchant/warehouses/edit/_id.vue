f810305d116922ba3bc2975d820453fd9578e96e
Hristiyan Yaprakov <hyaprakov@hyaprakovs-MacBook-Pro.local>
2019-02-25 18:45:43 +0000

    Warehouses - Index, create and edit, working and finished

--- website/pages/merchant/warehouses/edit/_id.vue
+++ website/pages/merchant/warehouses/edit/_id.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
+
         <page-title
-            title="Create a new warehouse"
+            :title="'Edit warehouse ' + (warehouse ? warehouse.name : 'loading...')"
         ></page-title>
 
-        <section class="section">
+        <section class="section" v-if="warehouse != null">
             <div class="container is-flex justify-center">
                 <div class="half-width">
                     <nuxt-link to="/merchant/warehouses" class="is-inline-block mb15">Back to all warehouses</nuxt-link>
@@ -14,7 +15,7 @@
                             <div class="field">
                                 <label class="label">Warehouse name:</label>
                                 <div class="control">
-                                    <input class="input" type="text" placeholder="Enter your warehouse name" v-model="name">
+                                    <input class="input" type="text" placeholder="Enter your warehouse name" v-model="warehouse.name">
                                 </div>
                             </div>
                             <div class="field">
@@ -22,16 +23,16 @@
                                 <div class="control">
                                     <input class="input" type="file" placeholder="Enter your warehouse name" @change="uploadFile">
                                 </div>
-                                <img class="small-img mt15" :src="image" alt="">
+                                <img class="small-img mt15" :src="warehouse.image" alt="">
                             </div>
                             <div class="field">
                                 <label class="label">Location coordinates:</label>
                                 <div class="control columns">
                                     <div class="column is-6">
-                                        <input class="input" type="text" placeholder="Enter latitude" v-model.number="location.latitude">
+                                        <input class="input" type="text" placeholder="Enter latitude" v-model.number="warehouse.location.latitude">
                                     </div>
                                     <div class="column is-6">
-                                        <input class="input" type="text" placeholder="Enter longitude" v-model.number="location.longitude">
+                                        <input class="input" type="text" placeholder="Enter longitude" v-model.number="warehouse.location.longitude">
                                     </div>
                                 </div>
                             </div>
@@ -42,10 +43,10 @@
                                 </p>
                                 <div class="control columns">
                                     <div class="column is-6">
-                                        <input class="input" type="number" placeholder="Enter X dim" v-model.number="dimensions.x">
+                                        <input class="input" type="number" placeholder="Enter X dim" v-model.number="warehouse.dimensions.x">
                                     </div>
                                     <div class="column is-6">
-                                        <input class="input" type="number" placeholder="Enter Y dim" v-model.number="dimensions.y">
+                                        <input class="input" type="number" placeholder="Enter Y dim" v-model.number="warehouse.dimensions.y">
                                     </div>
                                 </div>
                                 <p class="help is-size-8">
@@ -54,10 +55,10 @@
                                 <input 
                                     type="number" 
                                     class="input half-width mb10" 
-                                    v-for="(shelf, i) in dimensions.z"
+                                    v-for="(shelf, i) in warehouse.dimensions.z"
                                     :key="'shelf-' + i"
                                     :placeholder="'Enter shelf N' + (i + 1) + ' height'"
-                                    v-model.number="dimensions.z[i]">
+                                    v-model.number="warehouse.dimensions.z[i]">
 
                                 <a href="javascript:;" class="button is-link is-outlined is-smallish mt15" @click="addShelf()">
                                     <span>Add a shelf</span>
@@ -69,9 +70,8 @@
                                     <a 
                                         href="javascript:;"
                                         class="button is-link" 
-                                        :disabled="!can_submit"
-                                        @click.stop.prevent="addWarehouse()">
-                                        <span>Add warehouse</span>
+                                        @click.stop.prevent="updateWarehouse()">
+                                        <span>Update warehouse</span>
                                     </a>
                                 </div>
                             </div>
@@ -95,25 +95,11 @@ export default {
     },
     data: function () {
         return {
-            name: null,
-            image: null,
-            location: {
-                latitude: null,
-                longitude: null
-            },
-            dimensions: {
-                x: null,
-                y: null,
-                z: [
-                    0.0
-                ]
-            }
+            warehouseId: this.$nuxt._route.params.id,
+            warehouse: null,
         }
     },
     methods: {
-        addShelf: function () {
-            this.dimensions.z.push(this.dimensions.z[this.dimensions.z.length - 1])
-        },
         uploadFile: function (event) {
             let file = event.target.files[0]
             this.createImage(file);
@@ -123,48 +109,53 @@ export default {
             var reader = new FileReader();
 
             reader.onload = (e) => {
-                this.image = e.target.result;
+                this.warehouse.image = e.target.result;
             };
 
             reader.readAsDataURL(file);
         },
-        addWarehouse () {
-            if (this.can_submit) {
-                axios.
-                    post('http://localhost:9000/warehouse/', {
-                        name: this.name,
-                        image: this.image,
-                        location: this.location,
-                        dimensions: this.dimensions,
-                        items: []
-                    }, {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'username': this.$store.state.user.username,
-                        }
-                    })
-                    .then((res) => {
-                        console.log("Server response: ", res);
-    
-                        if (res.status == 200) {
-                            this.$router.push('/merchant/warehouses/' + res.data.warehouse._id).go(1)
-                        }
-                    })
-                    .catch(function(error) {
-                        console.error("Error adding document: ", error);
-                    });
-            }
+        getWarehouse () {
+            axios.
+                get('http://localhost:9000/warehouse/' + this.warehouseId, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then((res) => {
+                    console.log("Server response: ", res);
+                    
+                    this.warehouse = res.data.warehouse
+                })
+                .catch(function(error) {
+                    console.error("Error adding document: ", error);
+                });
+        },
+        updateWarehouse: function () {
+            axios.
+                post('http://localhost:9000/warehouse/', this.warehouse, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'username': this.$store.state.user.username,
+                    }
+                })
+                .then((res) => {
+                    console.log("Server response: ", res);
+
+                    if (res.status == 200) {
+                        this.$router.push('/merchant/warehouses/' + this.warehouseId).go(1)
+                    }
+                })
+                .catch(function(error) {
+                    console.error("Error adding document: ", error);
+                });
         }
     },
-    computed: {
-        can_submit: function () {
-            return this.name && this.location.latitude && this.location.longitude && this.dimensions.x && this.dimensions.y
-        }
+    mounted: function () {
+        this.getWarehouse()
     }
 };
 </script>
 
 <style lang="sass" scoped>
-
 
 </style>
