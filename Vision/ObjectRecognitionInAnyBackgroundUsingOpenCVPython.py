9c3e991ca31afe53eadc79049ca7865fd11a0a6c
cd6 <s1616573@sms.ed.ac.uk>
2019-03-30 15:52:08 +0000

    Orbs

--- Vision/ObjectRecognitionInAnyBackgroundUsingOpenCVPython.py
+++ Vision/ObjectRecognitionInAnyBackgroundUsingOpenCVPython.py
@@ -1,30 +1,53 @@
 import cv2
-import numpy
+import numpy as np
 
 # https://thecodacus.com/object-recognition-using-opencv-python/#.XJ55xJzgoeM
+# http://jematoscv.blogspot.com/2014/05/matching-features-with-orb-using-opencv.html
 
-detector=sift = cv2.SIFT() #cv2.xfeatures2d.SIFT_create() #cv2.SIFT()
-FLANN_INDEX_KDITREE=0
-flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
-flann=cv2.FlannBasedMatcher(flannParam,{})
+detector = cv2.ORB() #FeatureDetector_create("ORB")#cv2.xfeatures2d.SIFT_create() #cv2.SIFT()
+FLANN_INDEX_KDTREE = 0
+flannParam = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+flann = cv2.FlannBasedMatcher(flannParam, {})
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-trainImg=cv2.imread("bob_logo.jpg",0)
-trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
+print("flann")
+
+trainImg=cv2.imread("bob_logo.jpg")
+gray = cv2.cvtColor(trainImg, cv2.COLOR_BGR2GRAY)
+print("bob_logo")
+#trainKP=detector.detect(gray,None)
+print("detect")
+#trainKP,trainDesc=detector.compute(gray,trainKP)
+trainKP, trainDesc = detector.detectAndCompute(trainImg, None)
+print("compute")
 
 cam=cv2.VideoCapture(0)
 
-while True:
-    ret, QueryImgBGR=cam.read()
-    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
-    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
-    matches=flann.knnMatch(queryDesc,trainDesc,k=2)
 
-    goodMatch=[]
-    for m,n in matches:
-        if(m.distance<0.75*n.distance):
-            goodMatch.append(m)
+while True:
+    ret, QueryImgBGR = cam.read()
+    queryImg = cv2.cvtColor(QueryImgBGR, cv2.COLOR_BGR2GRAY)
+    print(queryImg)
+    queryKP, queryDesc = detector.detectAndCompute(queryImg, None)
+
+    if len(trainDesc) == 0:
+        cvError(0, "MatchFinder", "Train descriptor empty", __FILE__, __LINE__);
+    if len(queryDesc) == 0:
+        cvError(0, "MatchFinder", "Query descriptor empty", __FILE__, __LINE__);
+
+    #matches = flann.knnMatch(queryDesc, trainDesc, k=2)
+    matches = bf.match(queryDesc,trainDesc)
+    dist = [m.distance for m in matches]
+    thres_dist = (sum(dist) / len(dist)) * 0.5
+    goodMatch = [m for m in matches if m.distance < thres_dist]
+
+    #goodMatch=[]
+    #for m, n in matches:
+    #    if(m.distance<0.75*n.distance):
+    #        goodMatch.append(m)
 
     MIN_MATCH_COUNT=30
+    print(len(goodMatch))
     if(len(goodMatch)>=MIN_MATCH_COUNT):
         tp=[]
         qp=[]
@@ -33,16 +56,16 @@ while True:
             tp.append(trainKP[m.trainIdx].pt)
             qp.append(queryKP[m.queryIdx].pt)
 
-        tp,qp=np.float32((tp,qp))
+        tp, qp = np.float32((tp, qp))
 
-        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
+        H, status = cv2.findHomography(tp, qp, cv2.RANSAC, 3.0)
 
-        h,w=trainImg.shape
-        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
-        queryBorder=cv2.perspectiveTransform(trainBorder,H)
-        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+        h, w = trainImg.shape
+        trainBorder = np.float32([[[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]])
+        queryBorder = cv2.perspectiveTransform(trainBorder, H)
+        cv2.polylines(QueryImgBGR, [np.int32(queryBorder)], True, (0, 255, 0), 5)
     else:
-        print "Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
+        print "Not Enough match found- %d/%d"%(len(goodMatch), MIN_MATCH_COUNT)
     cv2.imshow('result', QueryImgBGR)
     if cv2.waitKey(10) == ord('q'):
         break
